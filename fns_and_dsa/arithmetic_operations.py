78019d2e19dda2021b0275af13b1c9d4f458ef9a
davidroi1 <vagba.belia1@gmail.com>
2025-06-07 22:50:09 +0000

    updating arithmetic operation python file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,8 @@
 def perform_operation(num1, num2, operation):
     if operation == 'divide':
-        try:
-            result = num1 / num2
-        except ZeroDivisionError:
-            return "error: impossible to divide number by zero"
-        else:
-            return result
+        if num2 == 0:
+            raise ZeroDivisionError("error: impossible to divide number by zero")
+        return num1 / num2
     elif operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
