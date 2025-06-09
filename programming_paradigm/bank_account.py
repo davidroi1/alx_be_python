6ffb2a2307756f033144f930b198169935742a6c
davidroi1 <vagba.belia1@gmail.com>
2025-06-09 23:50:16 +0000

    new submission for bank file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance=0) -> None:
+        self.account_balance = account_balance
+
+    def deposit(self, amount) -> None:
+        self.account_balance += amount
+
+    def withdraw(self, amount) -> bool:
+        if self.account_balance > 0 and amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f'Current Balance: ${self.account_balance}')
