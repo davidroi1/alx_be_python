c6bc939ab94d7f3d1198d7528fbd2072e4717bb8
davidroi1 <vagba.belia1@gmail.com>
2025-06-10 18:05:31 +0000

    update submission for two file

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,32 +1,6 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-def convert_to_celsius(fahrenheit):
-  temp = (fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
-  print(f'{fahrenheit}\N{DEGREE SIGN}F is {temp}\N{DEGREE SIGN}C')
-
-def convert_to_fahrenheit(celsius):
-  temp = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-  print(f'{celsius}\N{DEGREE SIGN}C is {temp}\N{DEGREE SIGN}F')
-
-
-try:
-  temp = float(input('Enter the temperature to convert:'))
-  unit = (input('Is this temperature in Celsius or Fahrenheit? (C/F): '))
-
-  if unit.upper() == 'C':
-    convert_to_fahrenheit(temp)
-  elif unit.upper() == 'F':
-    convert_to_celsius(temp)
-  else:
-    exit(1)
-except ValueError:
-  print('Invalid temperature. Please enter a numeric value.')
-
-
-"""FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-
 
 def convert_to_celsius(fahrenheit):
     #Convert Fahrenheit to Celsius.
@@ -48,7 +22,7 @@ def main():
             converted = convert_to_fahrenheit(temperature)
             print(f'{temperature}°C is {converted}°F')  # Fixed message
         else:
-            print('Invalid choice. Please enter "C" or "F".')"""
+            print('Invalid choice. Please enter "C" or "F".')
 
     except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
@@ -56,3 +30,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+"""FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+  temp = (fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
+  print(f'{fahrenheit}\N{DEGREE SIGN}F is {temp}\N{DEGREE SIGN}C')
+
+def convert_to_fahrenheit(celsius):
+  temp = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+  print(f'{celsius}\N{DEGREE SIGN}C is {temp}\N{DEGREE SIGN}F')
+
+
+try:
+  temp = float(input('Enter the temperature to convert:'))
+  unit = (input('Is this temperature in Celsius or Fahrenheit? (C/F): '))
+
+  if unit.upper() == 'C':
+    convert_to_fahrenheit(temp)
+  elif unit.upper() == 'F':
+    convert_to_celsius(temp)
+  else:
+    exit(1)
+except ValueError:
+  print('Invalid temperature. Please enter a numeric value.')"""
--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(10, 3), 13)
@@ -16,7 +16,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(16, -1), 15)
         self.assertEqual(self.calc.add(18, -20), -2)
     
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(10, 3), 7)
