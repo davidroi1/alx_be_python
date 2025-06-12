fdd9b47b5d6ea0eebc918c34bc75553e4db8485a
davidroi1 <vagba.belia1@gmail.com>
2025-06-12 22:45:50 +0000

    new submission for match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,10 +4,14 @@ match_operation = input("Choose the operation (+, -, *, /):")
 
 
 match match_operation:
-    case '+': print(f'The result is {num_1 + num_2}.')
-    case '*': print(f'The result is {num_1 * num_2}.')
+    case '+': 
+        print(f'The result is {num_1 + num_2}.')
+    case '*': 
+        print(f'The result is {num_1 * num_2}.')
     case '/': 
         if num_2 == 0:
             print("Cannot divide by zero.")
         else:
             print(f'The result is {num_1 / num_2}.')
+    case _:
+        print('Cannot find this instruction')
