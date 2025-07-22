8a12d628beea13854641da86771f61879909d2c6
davidroi1 <vagba.belia1@gmail.com>
2025-07-22 18:16:51 +0000

    oop submission for all task

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+from math import pi
+
+
+class Shape:
+    def area(self):
+        raise NotImplementedError()
+
+
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle:
+    def __init__(self, radius: int):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius**2
